a23a4802d1c089a7f87f9f1c633b375291ff033e
MrCj64 <sebas.cisgal@gmail.com>
2024-09-19 10:40:41 -0600

    Update dictionary.c

--- data-dictionary-main/dictionary.c
+++ data-dictionary-main/dictionary.c
@@ -2,8 +2,14 @@
 
 void initializeDataDictionary(const char *dictionaryNom) {
     long apuntador= -1;
+    
     printf("Initializing Data Dictionary...\n");
 
     FILE *dictionary = fopen(dictionaryNom,"wr");
+    
     fwrite(&apuntador,sizeof(apuntador),1,dictionary);
+
+    long currentDir = ftell(dictionary);
+
+    INVESTIGAR TERMINO Magic Numbers
 }

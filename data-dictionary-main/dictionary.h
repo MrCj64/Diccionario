39aed1c3a37c79d12bb8a7d7a1bd75225afd2b59
MrCj64 <sebas.cisgal@gmail.com>
2024-09-20 10:28:40 -0600

    Update dictionary.h

--- data-dictionary-main/dictionary.h
+++ data-dictionary-main/dictionary.h
@@ -4,6 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node
+{
+  char value[50];
+  struct Node *next;
+  long next;
+}NODE;
+
 #define EMPTY_POINTER -1
 
 int initializeDataDictionary(const char *dictionaryNom);
